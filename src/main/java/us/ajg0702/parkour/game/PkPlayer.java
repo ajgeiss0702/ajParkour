f3ca50ff0a33c7e871426cd1d4cf3497312eabb9
ajgeiss0702 <ajgeiss72@gmail.com>
2020-01-01 13:56:58 -0700

    should fix first few blocks are weird sometimes

--- src/main/java/us/ajg0702/parkour/game/PkPlayer.java
+++ src/main/java/us/ajg0702/parkour/game/PkPlayer.java
@@ -111,7 +111,7 @@ public class PkPlayer implements Listener {
 		}
 		Location tp = jumps.get(0).getTo();
 		teleporting = true;
-		p.teleport(new Location(tp.getWorld(), tp.getX()+0.5, tp.getY()+1.0, tp.getZ()+0.5));
+		p.teleport(new Location(tp.getWorld(), tp.getX()+0.5, tp.getY()+1.0, tp.getZ()+0.5, p.getLocation().getYaw(), p.getLocation().getPitch()));
 		Bukkit.getScheduler().scheduleSyncDelayedTask(m.main, new Runnable() {
 			public void run() {
 				teleporting = false;
